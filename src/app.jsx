d588f96d58f28a70d19911af0c578e7cc4053e0d
Farid Karimli <karimli.farid@icloud.com>
2024-06-14 00:40:55 -0400

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return <div>Hello, World!</div>;
+};
+
+createRoot(document.body).render(<App />);
